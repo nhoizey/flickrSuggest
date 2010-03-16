aacbffd8dbd44a47891b596e663ec79ee84ccd3e
Nicolas Hoizey <nicolas@hoizey.com>
2010-03-16 19:25:49 +0100

    Github link

--- inc/layout_page_bottom.inc.php
+++ inc/layout_page_bottom.inc.php
@@ -3,7 +3,7 @@
   require 'inc/close.inc.php';
   ?>
   <footer class="span-24">
-    <p>flickrSuggest is developed by <a href="http://www.gasteroprod.com/">Nicolas Hoizey</a> and released under the <a href="http://www.gnu.org/licenses/gpl.html">GNU/GPL v3 license</a>. You can <a href="">fork it on Github</a>.</p>
+    <p>flickrSuggest is developed by <a href="http://www.gasteroprod.com/">Nicolas Hoizey</a> and released under the <a href="http://www.gnu.org/licenses/gpl.html">GNU/GPL v3 license</a>. You can <a href="http://github.com/nhoizey/flickrSuggest">fork it on Github</a>.</p>
   </footer>
 </div>
 <script type="text/javascript" src="/js/behaviors.js"></script>
