10854990bd410197599c9bd7d9d1850d51498fce
Nicolas Hoizey <nicolas@hoizey.com>
2010-08-05 13:09:49 +0200

    JS quirks

--- js/behaviors.js
+++ js/behaviors.js
@@ -2,17 +2,25 @@ $(document).ready(function() {
   $('.ignore').bind('click', function() {
     var $this = $(this);
     var $photo = $this.parent().parent();
-    $photo.find('img').animate({ opacity: 0.25 }, 1000);
     $photo.append('<div class="status"><p>Ignoring this photo...</p><img src="img/indicator.gif" width="16" height="16" /></div>');
+    $photo.find('img').animate({ opacity: 0.25 }, 2000);
     var $href = $this.attr('href') + '&mode=ajax';
     $.get($href, function(data) {
       if (data == '1') {
-        $photo.hide('slow', function() { $(this).remove(); });
+        $photo.animate({ opacity: 0 }, 1000, function() {
+          $(this).remove();
+        });
       } else {
-        alert('Couldn\'t ignore this photo!' + "\n" + 'Message: ' + data);
+        $photo.find('.status').html('<p><strong>Couldn\'t ignore this photo!</strong></p><p>Message:<br />' + data + '</p>');
+        $photo.find('img').animate({ opacity: 0.75 }, 2000, function () {
+          $photo.find('.status').remove();
+        });
       }
     });
     return false;
   });
-  $('.pager').before('<p style="float: right; clear: left;"><a href="javascript:$(\'.ignore\').click();">ignore all</a></p>');
+  $('<p style="float: right; clear: left;"><a href="">ignore all</a></p>').insertBefore('.pager').find('a').bind('click', function () {
+    $('.ignore').click();
+    return false;
+  });
 });
