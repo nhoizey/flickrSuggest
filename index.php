58a97c185a85a2f173c54d80950e753e44c4ba29
Nicolas Hoizey <nicolas@hoizey.com>
2012-09-25 10:44:07 +0200

    Be nice at first use!

--- index.php
+++ index.php
@@ -5,20 +5,28 @@ require_once 'inc/photo.inc.php';
 require_once 'inc/favorites.inc.php';
 require_once 'Flickr/API.php';
 
+?>
+<h2>flickrSuggest</h2>
+<?php
 $myFavs = implode(', ', $db->getCol("SELECT photo_id FROM favorites WHERE user_nsid = '".FLICKR_USER_NSID."'"));
+if ($myFavs == '') {
+  ?>
+  <p>You need to go to the <a href="/update.php">update page</a> to start retrieving your favorites.</p>
+  <?php
+  require_once 'inc/layout_page_bottom.inc.php';
+  exit(0);
+}
 $total = $db->getOne("SELECT COUNT(DISTINCT photo_id) FROM favorites WHERE photo_id NOT IN (".$myFavs.")");
 $nbNeighbours = $db->getOne("SELECT COUNT(user_nsid) FROM users WHERE ignored=0") - 1;
 $nbIgnoredUsers = $db->getOne("SELECT COUNT(user_nsid) FROM users WHERE ignored=1");
 ?>
-
-<h2>flickrSuggest</h2>
 <p>These suggestions are based on favorites from <?php echo $nbNeighbours; ?> users that share at least <?php echo NEIGHBOURHOOD_DISTANCE; ?> favorites with me.
 <?php echo $nbIgnoredUsers; ?> users are ignored because they have at least <?php echo IGNORED_DISTANCE; ?> favorites that I ignored.</p>
 <h3>Favorites suggestions</h3>
 <?php
 $favoritesSuggestions = $db->getAll("SELECT DISTINCT photo_id, nb FROM favorites WHERE photo_id NOT IN (".$myFavs.") ORDER BY nb DESC, photo_id LIMIT 0,3");
 if ($total > 0) {
-  echo '<ol class="gallery">';  
+  echo '<ol class="gallery">';
   foreach($favoritesSuggestions as $data) {
   	echo getPhotoHTML($data['photo_id'], $data['nb']);
   }
@@ -43,7 +51,7 @@ if ($contactsListResponse && $contactsListResponse->attributes['stat'] == 'ok') 
 
 $contactsSuggestionsQuery = "SELECT DISTINCT photographer_nsid, COUNT(photo_id) AS nb FROM favorites WHERE user_nsid = '".FLICKR_USER_NSID."' AND photographer_nsid NOT IN (".$contactsString.") GROUP BY photographer_nsid HAVING nb >= ".CONTACTS_SUGGESTIONS_TRIGGER." ORDER BY nb DESC, photographer_nsid LIMIT 0,10";
 $contactsSuggestions = $db->getAll($contactsSuggestionsQuery);
-echo '<ol>';  
+echo '<ol>';
 foreach($contactsSuggestions as $contactNsid) {
   $contactInfoResponse = $flickr->callMethod('flickr.people.getInfo', array('email' => FLICKR_ACCOUNT_EMAIL, 'password' => FLICKR_ACCOUNT_PASSWORD, 'user_id' => $contactNsid['photographer_nsid']));
   if ($contactInfoResponse && $contactInfoResponse->attributes['stat'] == 'ok') {
